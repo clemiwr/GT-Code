6b8615a212970f5c19b75048fa4d5bec01bea9f2
stumpfi00 <maxiasd@icloud.com>
2024-03-21 07:50:03 +0100

    Bootstrap

--- GameTools/src/components/Sidebar.jsx
+++ GameTools/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 
 function Sidebar() {
@@ -15,9 +15,9 @@ function Sidebar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home"><i class="bi bi-house-fill"></i></Nav.Link>
-              <Nav.Link href="#link"><i class="bi bi-controller"></i></Nav.Link>
-              <Nav.Link href="#link"><i class="bi bi-mouse"></i></Nav.Link>
+              <Nav.Link href="#home"><i className="bi bi-house-fill"></i></Nav.Link>
+              <Nav.Link href="#link"><i className="bi bi-controller"></i></Nav.Link>
+              <Nav.Link href="#link"><i className="bi bi-mouse"></i></Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
