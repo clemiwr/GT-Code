ff464cc8abdc2f1a29cee8249fed2c627e983409
stumpfi00 <maximilian.stumpfer@sz-ybbs.ac.at>
2024-05-14 13:43:13 +0200

    changes

---
+++ GameTools/src/components/finalController.jsx
@@ -0,0 +1,4 @@
+import useGamepads from '../hooks/useGamepads';
+import { GamepadsProvider, GamepadsContext } from './GamepadsContext';
+
+export { useGamepads, GamepadsProvider, GamepadsContext };
--- GameTools/src/components/sites/ControllerTool.jsx
+++ GameTools/src/components/sites/ControllerTool.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import GamepadController from '../GamepadController';
 function ControllerTool() {
-    return ( 
-        <div style={{ color: '#e6e8db' }}>
-            <h1>Willkommen bei GameTools!</h1>
-            <p>Dies ist die Controller Tool.</p>
-        </div>
-     );
+    return (
+        <>
+            <div style={{ color: '#e6e8db' }}>
+                <h1>Willkommen bei GameTools!</h1>
+                <p>Dies ist die Controller Tool.</p>
+            </div>
+            <GamepadController />
+        </>
+    );
 }
 
 export default ControllerTool;
