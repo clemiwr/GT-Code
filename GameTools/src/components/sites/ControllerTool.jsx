bbfbd4fae0f9db8feda9f8fc6ce421cca8de3289
RobschCocovic <robertcovaci30@gmail.com>
2024-05-21 15:01:24 +0200

    frontend controller tool + accordion

--- GameTools/src/components/sites/ControllerTool.jsx
+++ GameTools/src/components/sites/ControllerTool.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import GamepadController from '../GamepadController';
+import Accordion from '../Accordion'; // Stellen Sie sicher, dass der Pfad zur Accordion-Komponente korrekt ist
+import '../../style/Controller.css';
+
 function ControllerTool() {
     return (
-        <>
-            <div style={{ color: '#e6e8db' }}>
-                <h1>Willkommen bei GameTools!</h1>
-                <p>Dies ist die Controller Tool.</p>
+        <div className="controller-tool">
+            <h1 className="controller-title">Willkommen bei GameTools!</h1>
+            <Accordion title="Beschreibung">
+                <p className="controller-description">
+                    Schließen Sie Ihren Gamecontroller an und erkunden Sie die vielfältigen Möglichkeiten unseres Tools. 
+                    Auf der rechten unteren Seite sehen Sie eine interaktive SVG-Grafik, die Ihre Eingaben in Echtzeit visualisiert.
+                    Jeder Tastendruck wird erkannt und die entsprechende Zone wird hervorgehoben, um Ihnen sofortiges Feedback zu geben.
+                </p>
+            </Accordion>
+            <div style={{ position: 'fixed', bottom: '10px', right: '10px' }}>
+                <GamepadController />
             </div>
-            <GamepadController />
-        </>
+        </div>
     );
 }
 
