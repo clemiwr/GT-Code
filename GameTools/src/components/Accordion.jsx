d79c7d6af32176743ac50880090de98d728ac334
RobschCocovic <robertcovaci30@gmail.com>
2024-05-28 14:48:36 +0200

    updated accordion

--- GameTools/src/components/Accordion.jsx
+++ GameTools/src/components/Accordion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import '../../style/Accordion.css'; // Stellen Sie sicher, dass der Pfad zur CSS-Datei korrekt ist
+import '../style/Accordion.css'; // Stellen Sie sicher, dass der Pfad zur CSS-Datei korrekt ist
 
 function Accordion({ title, children }) {
     const [isOpen, setIsOpen] = useState(false);
