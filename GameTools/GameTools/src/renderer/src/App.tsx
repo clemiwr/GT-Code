9a2f1668419e6f723867abcc12a052e43e73d3a3
stumpfi00 <maximilian.stumpfer@sz-ybbs.ac.at>
2024-06-04 13:35:58 +0200

    Update App.tsx

--- GameTools/GameTools/src/renderer/src/App.tsx
+++ GameTools/GameTools/src/renderer/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import './style/App.css'
 import Sidebar from './components/Sidebar'
