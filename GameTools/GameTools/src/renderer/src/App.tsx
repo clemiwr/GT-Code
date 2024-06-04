57c8395ef7f73db926b467ff5cf07053f453e7e1
stumpfi00 <maximilian.stumpfer@sz-ybbs.ac.at>
2024-06-04 13:00:31 +0200

    versuche

---
+++ GameTools/GameTools/src/renderer/src/App.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import './style/App.css'
+import Sidebar from './components/Sidebar'
+import Hohme from './components/sites/Hohme'
+import ControllerTool from './components/sites/ControllerTool'
+import DpiConverter from './components/sites/DpiConverter'
+import { Routes, Route } from 'react-router-dom'
+import Logo from './components/Logo'
+import { useState, useEffect } from 'react'
+import Login from './components/sites/Login'
+import { checkAuth } from './firebase'
+function App() {
+  const [isSignedIn, setisSignedIn] = useState(false)
+  useEffect(() => {
+    checkAuth().then((isAuthenticated) => {
+      setisSignedIn(isAuthenticated)
+    })
+  }, [])
+
+  return (
+    <>
+      {isSignedIn ? (
+        <div className="content-wrapper">
+          <Sidebar />
+          <Logo />
+          <div id="main-content">
+            <Routes>
+              <Route path="/" element={<Hohme />} />
+              <Route path="/DPI-Converter" element={<DpiConverter />} />
+              <Route path="/Controller-Tool" element={<ControllerTool />} />
+            </Routes>
+          </div>
+        </div>
+      ) : (
+        <Login isSignedIn={setisSignedIn} />
+      )}
+    </>
+  )
+}
+
+export default App
